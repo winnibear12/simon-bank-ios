dd698a3e5f8ae4d0b828818248b2b0e12130338e
Sangmin Lee (Simon) <winnibear12@gmail.com>
2023-05-25 20:56:32 -0400

    Add login Page

---
+++ myBank/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  myBank
+//
+//  Created by Simon Lee on 2023-05-23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+       
+       func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+           
+           window = UIWindow(frame: UIScreen.main.bounds)
+           window?.makeKeyAndVisible()
+           window?.backgroundColor = .orange
+           window?.rootViewController = LoginViewController()
+
+           return true
+       }
+
+    
+
+}
+
