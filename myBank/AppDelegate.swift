a64f004d65badb245dc1dc8c34f0f7c3d0a40ce6
Sangmin Lee (Simon) <winnibear12@gmail.com>
2023-05-27 13:49:36 -0400

    Add onboarding view controller

--- myBank/AppDelegate.swift
+++ myBank/AppDelegate.swift
@@ -11,13 +11,15 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
        
        func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
            
            window = UIWindow(frame: UIScreen.main.bounds)
            window?.makeKeyAndVisible()
            window?.backgroundColor = .orange
-           window?.rootViewController = LoginViewController()
+//           window?.rootViewController = LoginViewController()
+           window?.rootViewController = OnboardingContainerViewController()
 
            return true
        }
