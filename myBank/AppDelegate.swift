99d86f25e22d1ec428ec5fa5e34727cb07e593a9
Sangmin Lee (Simon) <winnibear12@gmail.com>
2023-05-28 00:17:20 -0400

    Add tab bar

--- myBank/AppDelegate.swift
+++ myBank/AppDelegate.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+let appColor: UIColor = .systemTeal
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -15,21 +17,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let loginViewController = LoginViewController()
     let onboardingContainerViewController = OnboardingContainerViewController()
     let dummyViewController = DummyViewController()
+    let mainViewController = MainViewController()
        
    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
        
        window = UIWindow(frame: UIScreen.main.bounds)
        window?.makeKeyAndVisible()
-       window?.backgroundColor = .orange
+       window?.backgroundColor = .white
            
        //window?.rootViewController = OnboardingContainerViewController()
        loginViewController.delegate = self
        onboardingContainerViewController.delegate = self
        dummyViewController.logoutDelegate = self
        
-       window?.rootViewController = loginViewController
+       window?.rootViewController = mainViewController
        
 //       window?.rootViewController = onboardingContainerViewController
+       
+       mainViewController.selectedIndex = 1
+       
        return true
    }
 
