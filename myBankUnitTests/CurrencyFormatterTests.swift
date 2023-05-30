c0ecfb48a68cfda0452f3b118c428162e6c76bf0
Sangmin Lee (Simon) <winnibear12@gmail.com>
2023-05-29 23:19:33 -0400

    test: Add unit tests for CurrencyFormatter

---
+++ myBankUnitTests/CurrencyFormatterTests.swift
@@ -0,0 +1,38 @@
+//
+//  CurrencyFormatterTests.swift
+//  myBankUnitTests
+//
+//  Created by Simon Lee on 2023-05-29.
+//
+
+import Foundation
+import XCTest
+
+@testable import myBank
+
+class CurrencyFormatterTests: XCTestCase {
+    var formatter: CurrencyFormatter!
+    
+    override func setUp() {
+        super.setUp()
+        formatter = CurrencyFormatter()
+    }
+    
+    func testBreakDollarsIntoCents() throws {
+        let result = formatter.breakIntoDollarsAndCents(929466.23)
+        XCTAssertEqual(result.0, "929,466")
+        XCTAssertEqual(result.1, "23")
+    }
+    
+
+    func testDollarsFormatted() throws {
+        let result = formatter.dollarsFormatted(929466.23)
+        XCTAssertEqual(result, "$929,466.23")
+    }
+
+
+    func testZeroDollarsFormatted() throws {
+        let result = formatter.dollarsFormatted(0.00)
+        XCTAssertEqual(result, "$0.00")
+    }
+}
