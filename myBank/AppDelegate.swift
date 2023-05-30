76b4cc17ae9b3b75eeb6f49afcf3788ba6987ef6
Sangmin Lee (Simon) <winnibear12@gmail.com>
2023-05-30 00:31:28 -0400

    refactor: General AppDelegate cleanup

--- myBank/AppDelegate.swift
+++ myBank/AppDelegate.swift
@@ -11,44 +11,51 @@ let appColor: UIColor = .systemTeal
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
     
     let loginViewController = LoginViewController()
     let onboardingContainerViewController = OnboardingContainerViewController()
     let dummyViewController = DummyViewController()
     let mainViewController = MainViewController()
-       
-   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-       
-       window = UIWindow(frame: UIScreen.main.bounds)
-       window?.makeKeyAndVisible()
-       window?.backgroundColor = .white
-           
-       loginViewController.delegate = self
-       onboardingContainerViewController.delegate = self
     
-       let vc = mainViewController
-       vc.setStatusBar()
-       
-       UINavigationBar.appearance().isTranslucent = false
-       UINavigationBar.appearance().backgroundColor = appColor
-       
-       window?.rootViewController = vc
-       
-       return true
-   }
-
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .white
+        
+        loginViewController.delegate = self
+        onboardingContainerViewController.delegate = self
+        
+        displayLogin()
+        return true
+    }
+    
+    private func displayLogin() {
+        setRootViewController(loginViewController)
+    }
+    
+    private func displayNextScreen() {
+        if LocalState.hasOnboarded {
+            prepMainView()
+            setRootViewController(mainViewController)
+        } else {
+            setRootViewController(onboardingContainerViewController)
+        }
+    }
+    
+    private func prepMainView() {
+        mainViewController.setStatusBar()
+        UINavigationBar.appearance().isTranslucent = false
+        UINavigationBar.appearance().backgroundColor = appColor
+    }
+    
 }
 
 extension AppDelegate: LoginViewControllerDelegate{
     func didLogin() {
-        if LocalState.hasOnboarded{
-            setRootViewController(mainViewController)
-        } else{
-            setRootViewController(onboardingContainerViewController)
-        }
-        
+        displayNextScreen()
     }
     
     
@@ -57,6 +64,7 @@ extension AppDelegate: LoginViewControllerDelegate{
 extension AppDelegate: OnboardingContainerViewControllerDelegate{
     func didFinishOnboarding() {
         LocalState.hasOnboarded = true
+        prepMainView()
         setRootViewController(mainViewController)
     }
 }
@@ -70,23 +78,23 @@ extension AppDelegate: LogoutDelegate {
 
 extension AppDelegate{
     func setRootViewController(_ vc: UIViewController, animated: Bool = true) {
-            guard animated, let window = self.window else {
-                self.window?.rootViewController = vc
-                self.window?.makeKeyAndVisible()
-                return
-            }
-
-            window.rootViewController = vc
-            window.makeKeyAndVisible()
-            UIView.transition(with: window,
-                              duration: 0.8,
-                              options: .transitionCrossDissolve,
-                              animations: nil,
-                              completion: nil)
+        guard animated, let window = self.window else {
+            self.window?.rootViewController = vc
+            self.window?.makeKeyAndVisible()
+            return
         }
+        
+        window.rootViewController = vc
+        window.makeKeyAndVisible()
+        UIView.transition(with: window,
+                          duration: 0.8,
+                          options: .transitionCrossDissolve,
+                          animations: nil,
+                          completion: nil)
+    }
 }
 
-    
-    
+
+
 
 
