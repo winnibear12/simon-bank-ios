2fd143a4f09448c87cbfeed055848d48b11c7dc6
Sangmin Lee (Simon) <winnibear12@gmail.com>
2023-05-29 22:09:18 -0400

    feat: Add CurrencyFormatter

--- myBank/AccountSummary/Cells/AccountSummaryCell.swift
+++ myBank/AccountSummary/Cells/AccountSummaryCell.swift
@@ -19,6 +19,10 @@ class AccountSummaryCell: UITableViewCell{
     struct ViewModel {
         let accountType: AccountType
         let accountName: String
+        let balance: Decimal
+        var balanceAsAttributedString: NSAttributedString {
+               return CurrencyFormatter().makeAttributedCurrency(balance)
+           }
     }
 
     let viewModel: ViewModel? = nil
@@ -75,7 +79,7 @@ extension AccountSummaryCell{
 
         balanceAmountLabel.translatesAutoresizingMaskIntoConstraints = false
         balanceAmountLabel.textAlignment = .right
-        balanceAmountLabel.attributedText = makeFormattedBalance(dollars: "929,466", cents: "23")
+        balanceAmountLabel.attributedText = makeFormattedBalance(dollars: "XXX,XXX", cents: "XX")
 
         
         chevronImageView.translatesAutoresizingMaskIntoConstraints = false
@@ -136,6 +140,7 @@ extension AccountSummaryCell {
         
         typeLabel.text = vm.accountType.rawValue
         nameLabel.text = vm.accountName
+        balanceAmountLabel.attributedText = vm.balanceAsAttributedString
 
         switch vm.accountType{
         case .Banking:
